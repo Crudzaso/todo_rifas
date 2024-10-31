dfb6abad0d13701f77560de247adba49e6f25789
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-10-30 19:14:22 -0500

    fix: advances in dashboard

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,247 @@
+<div id="kt_header" class="header py-6 py-lg-0" data-kt-sticky="true" data-kt-sticky-name="header" data-kt-sticky-offset="{lg: '300px'}">
+    <!--begin::Container-->
+    <div class="header-container container-xxl">
+        <!--begin::Page title-->
+        <div class="page-title d-flex flex-column align-items-start justify-content-center flex-wrap me-lg-20 py-3 py-lg-0 me-3">
+            <!--begin::Heading-->
+            <h1 class="d-flex flex-column text-gray-900 fw-bold my-1">
+                <span class="text-white fs-1">¡La suerte en tus manos!</span>
+                <small class="text-gray-600 fs-6 fw-normal pt-2">Bienvenido a TodoRifas</small>
+            </h1>
+            <!--end::Heading-->
+        </div>
+        <!--end::Page title=-->
+        <!--begin::Wrapper-->
+        <div class="d-flex align-items-center flex-wrap">
+            <!--begin::Action-->
+            <div class="d-flex align-items-center py-3 py-lg-0">
+                <!--begin::Item-->
+                <div class="me-3">
+                    <a href="#" class="btn btn-icon btn-custom btn-active-color-primary position-relative" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+                        <i class="ki-duotone ki-notification-bing fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                            <span class="path3"></span>
+                        </i>
+                        <span class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
+                    </a>
+                    <!--begin::Menu-->
+                    <div class="menu menu-sub menu-sub-dropdown menu-column w-350px w-lg-375px" data-kt-menu="true" id="kt_menu_notifications">
+                        <!--begin::Heading-->
+                        <div class="d-flex flex-column bgi-no-repeat rounded-top" style="background-image:url('assets/media/misc/menu-header-bg.jpg')">
+                            <!--begin::Title-->
+                            <h3 class="text-white fw-semibold px-9 mt-10 mb-6">Notificaciones
+                            <span class="fs-8 opacity-75 ps-3">0 Reportes</span></h3>
+                            <!--end::Title-->
+                            <!--begin::Tabs-->
+                            <ul class="nav nav-line-tabs nav-line-tabs-2x nav-stretch fw-semibold px-9">
+                                <li class="nav-item">
+                                    <a class="nav-link text-white opacity-75 opacity-state-100 pb-4" data-bs-toggle="tab" href="#kt_topbar_notifications_1">Alertas</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white opacity-75 opacity-state-100 pb-4 active" data-bs-toggle="tab" href="#kt_topbar_notifications_2">Actualizaciones</a>
+                                </li>
+                            </ul>
+                            <!--end::Tabs-->
+                        </div>
+                        <!--end::Heading-->
+                        <!--begin::Tab content-->
+                        <div class="tab-content">
+                            <!--begin::View more-->
+                                <div class="py-3 text-center border-top">
+                                    <a href="pages/user-profile/activity.html" class="btn btn-color-gray-600 btn-active-color-primary">View All
+                                    <i class="ki-duotone ki-arrow-right fs-5">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                    </i></a>
+                                </div>
+                                <!--end::View more-->
+                            </div>
+                            <!--end::Tab panel-->
+                            <!--begin::Tab panel-->
+                            <div class="tab-pane fade show active" id="kt_topbar_notifications_2" role="tabpanel">
+                                <!--begin::Wrapper-->
+                                <div class="d-flex flex-column px-9">
+                                    <!--begin::Section-->
+                                    <div class="pt-10 pb-0">
+                                        <!--begin::Title-->
+                                        <h3 class="text-gray-900 text-center fw-bold">Get Pro Access</h3>
+                                        <!--end::Title-->
+                                        <!--begin::Text-->
+                                        <div class="text-center text-gray-600 fw-semibold pt-1">Outlines keep you honest. They stoping you from amazing poorly about drive</div>
+                                        <!--end::Text-->
+                                        <!--begin::Action-->
+                                        <div class="text-center mt-5 mb-9">
+                                            <a href="#" class="btn btn-sm btn-primary px-6" data-bs-toggle="modal" data-bs-target="#kt_modal_upgrade_plan">Upgrade</a>
+                                        </div>
+                                        <!--end::Action-->
+                                    </div>
+                                    <!--end::Section-->
+                                    <!--begin::Illustration-->
+                                    <div class="text-center px-4">
+                                        <img class="mw-100 mh-200px" alt="image" src="assets/media/illustrations/sigma-1/1.png" />
+                                    </div>
+                                    <!--end::Illustration-->
+                                </div>
+                                <!--end::Wrapper-->
+                            </div>
+                            <!--end::Tab panel-->
+                        </div>
+                        <!--end::Tab content-->
+                    </div>
+                    <!--end::Menu-->
+                </div>
+                <!--end::Item-->
+                <!--begin::Item-->
+                <div class="me-3">
+                    <a href="#" class="btn btn-icon btn-custom btn-active-color-primary" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+                        <i class="ki-duotone ki-user fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
+                    </a>
+                    <!--begin::User account menu-->
+                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-3">
+                            <div class="menu-content d-flex align-items-center px-3">
+                                <!--begin::Avatar-->
+                                <div class="symbol symbol-50px me-5">
+                                    <img alt="Logo" src="{{asset('images/todo_rifas_pet.png')}}" />
+                                </div>
+                                <!--end::Avatar-->
+                                <!--begin::Username-->
+                                <div class="d-flex flex-column">
+                                    <div class="fw-bold d-flex align-items-center fs-5">Max Smith</div>
+                                    <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">max@kt.com</a>
+                                </div>
+                                <!--end::Username-->
+                            </div>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu separator-->
+                        <div class="separator my-2"></div>
+                        <!--end::Menu separator-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-5">
+                            <a href="account/overview.html" class="menu-link px-5">Mi perfil</a>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-5">
+                            <a href="apps/projects/list.html" class="menu-link px-5">
+                                <span class="menu-text">Mis tickets</span>
+                                <span class="menu-badge">
+                                    <span class="badge badge-light-danger badge-circle fw-bold fs-7">3</span>
+                                </span>
+                            </a>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-5">
+                            <a href="account/statements.html" class="menu-link px-5">Facturas</a>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu separator-->
+                        <div class="separator my-2"></div>
+                        <!--end::Menu separator-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-5 my-1">
+                            <a href="account/settings.html" class="menu-link px-5">Configuración de la cuenta</a>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-5">
+                            <a href="authentication/layouts/corporate/sign-in.html" class="menu-link px-5">Salir</a>
+                        </div>
+                        <!--end::Menu item-->
+                    </div>
+                    <!--end::User account menu-->
+                </div>
+                <!--end::Item-->
+                <!--begin::Theme mode-->
+                <div class="d-flex align-items-center me-3">
+                    <!--begin::Menu toggle-->
+                    <a href="#" class="btn btn-icon btn-custom btn-active-color-primary" data-kt-menu-trigger="{default:'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+                        <i class="ki-duotone ki-night-day theme-light-show fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                            <span class="path3"></span>
+                            <span class="path4"></span>
+                            <span class="path5"></span>
+                            <span class="path6"></span>
+                            <span class="path7"></span>
+                            <span class="path8"></span>
+                            <span class="path9"></span>
+                            <span class="path10"></span>
+                        </i>
+                        <i class="ki-duotone ki-moon theme-dark-show fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
+                    </a>
+                    <!--begin::Menu toggle-->
+                    <!--begin::Menu-->
+                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-title-gray-700 menu-icon-gray-500 menu-active-bg menu-state-color fw-semibold py-4 fs-base w-150px" data-kt-menu="true" data-kt-element="theme-mode-menu">
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-3 my-0">
+                            <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="light">
+                                <span class="menu-icon" data-kt-element="icon">
+                                    <i class="ki-duotone ki-night-day fs-2">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                        <span class="path3"></span>
+                                        <span class="path4"></span>
+                                        <span class="path5"></span>
+                                        <span class="path6"></span>
+                                        <span class="path7"></span>
+                                        <span class="path8"></span>
+                                        <span class="path9"></span>
+                                        <span class="path10"></span>
+                                    </i>
+                                </span>
+                                <span class="menu-title">Light</span>
+                            </a>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-3 my-0">
+                            <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="dark">
+                                <span class="menu-icon" data-kt-element="icon">
+                                    <i class="ki-duotone ki-moon fs-2">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                    </i>
+                                </span>
+                                <span class="menu-title">Dark</span>
+                            </a>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-3 my-0">
+                            <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="system">
+                                <span class="menu-icon" data-kt-element="icon">
+                                    <i class="ki-duotone ki-screen fs-2">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                        <span class="path3"></span>
+                                        <span class="path4"></span>
+                                    </i>
+                                </span>
+                                <span class="menu-title">System</span>
+                            </a>
+                        </div>
+                        <!--end::Menu item-->
+                    </div>
+                    <!--end::Menu-->
+                </div>
+                <!--end::Theme mode-->
+                
+            </div>
+            <!--end::Action-->
+        </div>
+        <!--end::Wrapper-->
+    </div>
+    <!--end::Container-->
+    <div class="header-offset"></div>
+</div>
