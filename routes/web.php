8a7fded667ed704bfe906772fb24dc5d5182e4ed
Njco1 <Nicolasaf17@gmail.com>
2024-11-15 10:15:24 -0500

    Add routes for the user views

--- routes/web.php
+++ routes/web.php
@@ -74,3 +74,10 @@ Route::middleware([
     })->name('dashboard');
 });
 
+
+Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
+Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
+Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
+Route::get('/admin/users/{id}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
+Route::put('/admin/users/{id}', [UserController::class, 'update'])->name('admin.users.update');
+Route::delete('/admin/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
