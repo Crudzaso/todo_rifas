36d1fc8a1ca93954fba4396a0145a77abc65dc13
Nar Mena <narciris.tech@gmail.com>
2024-11-21 17:00:28 -0500

    add winner method

--- app/Http/Controllers/winnerController.php
+++ app/Http/Controllers/winnerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\RaffleEntries;
+use App\Models\Winner;
 use Illuminate\Support\Facades\Http;
 
 class winnerController extends Controller
@@ -10,12 +11,19 @@ class winnerController extends Controller
 
     public function showWinner()
     {
+        // Definir $winners y $pastWinners al inicio del método
+        $winners = [];
+        $pastWinners = Winner::orderBy('lottery_date', 'desc')->get();
+
         // 1. Consumir la API de loterías
         $apiUrl = "https://api-resultadosloterias.com/api/results";
         $response = Http::get($apiUrl);
 
         if ($response->failed()) {
-            return view('lottery.winner', ['error' => 'No se pudieron obtener los resultados de la lotería.']);
+            return view('lottery.winner', [
+                'error' => 'No se pudieron obtener los resultados de la lotería.',
+                'pastWinners' => $pastWinners // Incluir pastWinners incluso en caso de error
+            ]);
         }
 
         // Decodificar la respuesta JSON
@@ -24,23 +32,33 @@ class winnerController extends Controller
         // 2. Obtener números de los participantes registrados
         $entries = RaffleEntries::select('id', 'number')->get();
 
-        $winners = [];
-
         // 3. Comparar las últimas tres cifras del resultado con los números registrados
         foreach ($lotteryResults as $lottery) {
             $lastThreeDigits = substr($lottery['result'], -3);
 
             foreach ($entries as $entry) {
                 if ($entry->number === $lastThreeDigits) {
-                    $winners[] = [
+                    $winnerName = $entry->user->name;
+                    Winner::create([
+                        'participant_name' => $winnerName,
                         'lottery' => $lottery['lottery'],
-                        'winning_number' => $lastThreeDigits
+                        'winning_number' => $lastThreeDigits,
+                        'lottery_date' => $lottery['date'],
+                    ]);
+
+                    $winners[] = [
+                        'participant_name' => $winnerName,
+                        'lottery' => $lottery['lottery'],
+                        'winning_number' => $lastThreeDigits,
                     ];
                 }
             }
         }
 
-        // 4. Retornar los datos a la vista
-        return view('lottery.winner', ['winners' => $winners]);
+        // Retornar la vista con ambas variables
+        return view('lottery.winner', [
+            'winner' => $winners,
+            'pastWinners' => $pastWinners
+        ]);
     }
 }
